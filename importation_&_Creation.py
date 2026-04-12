9c1a79eb62e44667b914082e6e174e7336734f72
Yash Petkar <ritikpetkar44@gmail.com>
2026-04-12 12:56:29 +0530

    Add files via upload
    How to create numpy array .
    How to import numpy modules

---
+++ importation_&_Creation.py
@@ -0,0 +1,22 @@
+"""
+How to import numpy module
+1.Numpy=> Numerical Python
+2.Numpy is a library in Python that provides support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays.
+It is widely used in scientific computing, data analysis, and machine learning.
+
+"""
+#importing numpy module
+
+import numpy as np
+
+"""Creating numpy arrays"""
+
+a= np.array([1, 2, 3, 4, 5])
+
+"""
+1.The np.array() function is used to create a numpy array from a list.
+2.The argument passed to np.array() is a list of values that you want to include in the array
+
+"""
+#Now we can print the array to see its contents
+print(a)
